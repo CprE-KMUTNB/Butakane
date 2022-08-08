de1fe0da9be74117034ae7b2f5461107bb78466f
nonthawat-kmutnb <s6401012610018@email.kmutnb.ac.th>
2022-08-08 10:07:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn MERN
+          Learning MERN
         </a>
       </header>
     </div>
