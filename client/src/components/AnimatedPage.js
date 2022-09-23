6d99a248c058a248cd186ec7cedabdda64ba5499
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2022-09-23 13:01:13 +0700

    HomePage and PageAnimation

--- client/src/components/AnimatedPage.js
+++ client/src/components/AnimatedPage.js
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion';
 const AnimatedPage = ({children}) => {
 
     const animations = {
-        initial: {opacity: 0, y: -10},
+        initial: {opacity: 0, y: 0},
         animate: {opacity: 1, y: 0},
-        exit: {opacity: 0, y: 10},
+        exit: {opacity: 0, y: 0},
         transition: { duration: 100 }
     }
 
