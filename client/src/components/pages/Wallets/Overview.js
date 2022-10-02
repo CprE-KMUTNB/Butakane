818f0e036871d0e4ad2d13b8af4dbe8076a81688
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2022-10-02 21:31:03 +0700

    Overview 02.10.23

--- client/src/components/pages/Wallets/Overview.js
+++ client/src/components/pages/Wallets/Overview.js
@@ -1,10 +1,26 @@
 import '../../css/wallet.css'
 
 const Overview = () => {
+
   return (
     <div className="wallet-content-page">
       <div className="wallet-content">
-        <div>Overview</div>
+          <div className="overview-area">
+            <div className="overview-area-items">
+              <div className="overview-area-item-1">
+                <div className="item-balance"></div>
+                <div className="items-wallet-action">
+                  <div className="items-wallet-action-left">
+                    <div className="item-wallet-income"></div>
+                    <div className="item-wallet-outcome"></div>
+                  </div>
+                  <div className="item-mydebt"></div>
+                </div>
+              </div>
+              <div className="overview-area-item-2"></div>
+              <div className="overview-area-item-3"></div>
+            </div>
+          </div>
       </div>
     </div>
   )
