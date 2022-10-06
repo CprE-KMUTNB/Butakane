3d3ade11667089a930596776694aae5e1ff4f470
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2022-10-06 10:01:12 +0700

    myDebt Components

---
+++ client/src/components/LendModal.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Modal, Input, Button, Text } from "@nextui-org/react";
+import { WalletIcon } from './userIcon/WalletIcon';
+import { DetailsIcon } from "./userIcon/DetailsIcon"
+import { UserIcon } from './userIcon/UserIcon';
+
+const LendModal = () => {
+
+    const [visible, setVisible] = React.useState(false);
+    const handler = () => setVisible(true);
+    const closeHandler = () => {
+        setVisible(false);
+        console.log("closed");
+    };
+
+  return (
+    <>
+        <button className="lend-button" onClick={handler}>
+            ให้ยืม
+        </button>
+        <Modal
+            closeButton
+            blur
+            aria-labelledby="modal-title"
+            open={visible}
+            onClose={closeHandler}
+        >
+            <Modal.Header>
+            <Text id="modal-title" size={18}>
+                ให้ยืมเงิน
+            </Text>
+            </Modal.Header>
+            <Modal.Body>
+            <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="จำนวน"
+                contentLeft={<WalletIcon fill="currentColor" />}
+            />
+            <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="ผู้ยืม"
+                contentLeft={<UserIcon fill="currentColor" />}
+            />
+            <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="รายละเอียด"
+                contentLeft={<DetailsIcon fill="currentColor" />}
+            />
+            </Modal.Body>
+            <Modal.Footer>
+            <Button auto flat color="error" onClick={closeHandler}>
+                Close
+            </Button>
+            <Button auto>
+                บันทึก
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
+  )
+}
+
+export default LendModal
--- client/src/components/pages/Wallets/MyDebt.js
+++ client/src/components/pages/Wallets/MyDebt.js
@@ -1,4 +1,7 @@
 import '../../css/wallet.css'
+import { Text } from '@nextui-org/react'
+import BorrowModal from '../../BorrowModal'
+import LendModal from '../../LendModal'
 
 const MyDebt = () => {
   return (
@@ -8,16 +11,43 @@ const MyDebt = () => {
           <div className="mydebt-area">
             <div className="debt-area-items">
               <div className="debt-balance">
-                <div className="borrow-balance"></div>
-                <div className="lend-balance"></div>
+                <div className="borrow-balance">
+                  <p>Borrow - Balance</p>
+                  <h2>999</h2>
+                </div>
+                <div className="lend-balance">
+                  <p>Lend - Balance</p>
+                  <h2>999</h2>
+                </div>
               </div>
               <div className="debt-button">
-                <button className="borrow-button">ขอยืม</button>
-                <button className="lend-button">ให้ยืม</button>
+                <BorrowModal />
+                <LendModal />
               </div>
             </div>          
           </div>
         </div>
+        <div className="detail-title">
+          <span>รายการ</span>
+        </div>
+
+        <div className="detail-lists">
+          <div className="des-detail-list">
+              <span>ยืมเงินจาก USER1 - Lorem ipsum dolor sit amet.</span>
+          </div>
+          <div className="amount-detail-list">
+            <div className="amount-detail-lists">
+              <div className="amount-detail">
+                <Text size="$xl" color="success">+ 200</Text>
+              </div>
+              <div className="timestamp-detail">
+                  <Text>6/10/2565</Text>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   )
