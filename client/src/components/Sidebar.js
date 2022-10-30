38922154840e833930a670b4fef6203440e81dd5
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2022-10-30 19:32:31 +0700

    TH LANGUAGE

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -10,9 +10,9 @@ const Sidebar = () => {
   return (
     <aside>
         <ul className="side-item">
-            <li><NavLink to="Overview" className="side-text" style={({ isActive }) => isActive ? LinkActive : undefined}><FaGlobe className="FontIcon" /><span>Overview</span></NavLink></li>
-            <li><NavLink to="MyDebt" className="side-text" style={({ isActive }) => isActive ? LinkActive : undefined}><FaCoins className="FontIcon" /><span>MyDebt</span></NavLink></li>
-            <li><NavLink to="MyGoal" className="side-text" style={({ isActive }) => isActive ? LinkActive : undefined}><FaTrophy className="FontIcon" /><span>MyGoal</span></NavLink></li>
+            <li><NavLink to="Overview" className="side-text" style={({ isActive }) => isActive ? LinkActive : undefined}><FaGlobe className="FontIcon" /><span>ภาพรวม</span></NavLink></li>
+            <li><NavLink to="MyDebt" className="side-text" style={({ isActive }) => isActive ? LinkActive : undefined}><FaCoins className="FontIcon" /><span>หนี้สิน</span></NavLink></li>
+            <li><NavLink to="MyGoal" className="side-text" style={({ isActive }) => isActive ? LinkActive : undefined}><FaTrophy className="FontIcon" /><span>เป้าหมาย</span></NavLink></li>
         </ul>
     </aside>
   )
